11c3918c59eed6b79d40904ab260ffdf45fdd3e6
juanmi <juanmiguel@existo.es>
2018-03-22 16:24:48 +0100

    Maquetación de noticias y noticias-detalle

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,4 @@ app.use ('/sitemap', require ('./sitemap'));
 app.use ('/template', require ('./template'));
 app.use ('/editor', require ('./editor'));
 app.use ('/noticias', require ('./noticias'));
-app.use ('/noticias-lista', require ('./noticias-lista'));
-app.use ('/noticias-detalle', require ('./noticias-detalle'));
 app.use ('/proyectos', require ('./proyectos'));
